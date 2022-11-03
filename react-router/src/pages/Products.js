9332930f0028896606fc995031cfd54595113089
Sushmi-3 <sushmisumi3@gmail.com>
2022-11-03 03:04:19 -0700

    react-router

---
+++ react-router/src/pages/Products.js
@@ -0,0 +1,21 @@
+const Products = () => {
+    return (
+    <section>
+        <h1>The Products Page</h1>
+    <ul>
+        <li>
+           A Book
+        </li>
+        <li>
+           Camera
+        </li>
+        <li>
+           Pizza
+        </li>
+    </ul>
+    </section>
+
+    );
+}
+
+export default Products;
